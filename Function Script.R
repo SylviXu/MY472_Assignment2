33f7ddd9eae7cc9d20cbe8ea241380ac6d7ca90a
SylviXu <x.xui78@lse.ac.uk>
2023-10-30 22:19:29 +0000

    Define a single-variable function.

---
+++ Function Script.R
@@ -0,0 +1,41 @@
+#Load the dplyr package.
+library(dplyr)
+#Load the ggplot2 package.
+library(ggplot2)
+#Load the lubridate package.
+library(lubridate)
+
+#Load the dataset "economics".
+data("economics")
+
+#Define a function that filters for the highest per capita consumption value in a given year.
+#Add a variable y2 to make it output the month-on-month change in the broad unemployment rate for the selected year.
+transformed_economics <- function(y1, y2){
+  #Transform 1.
+  transformed_data <- economics %>% 
+    #Add a new colunm to display per capita consumption.
+    mutate(per_capita_consumption = economics$pce*10^9/(economics$pop*1000)) %>%
+    #Filter the row with the highest per capita consumption.
+    filter(year(as.Date(date)) == y1) %>% 
+    filter(per_capita_consumption == max(per_capita_consumption)) %>%
+    #Show the final tidy chart using 'summarise()' function. 
+    summarise(Year = y1, 
+              Date = date, 
+              Max_pcc = per_capita_consumption)
+  #Transform 2.
+  transformed_data2 <- economics %>%
+    #Add a column showing the month-on-month change in the broad unemployment rate.
+    mutate(unemployment_change = (economics$unemploy/economics$pop)*100 - lag(economics$unemploy/economics$pop)*100) %>%
+    #Filter out all data for the selected year. 
+    filter(year(date) == y2) %>%
+    #Remove the first month's data as the month-on-month change has no comparator in the first month.
+    filter(!is.na(unemployment_change)) %>%
+    group_by(date) %>%
+    #Show the final tidy chart.
+    summarise(unemployment_change)
+  result <- list(transformed_data, transformed_data2)
+  return(result)
+}
+
+#Preview the first few rows of transformed data.
+print(transformed_economics(1980, 2012))
